8bd459888d8e4cc95c505a0e5e69b8fad288084d
Rafael Vargas <rafael.vargas@dotz.com.br>
2017-08-28 13:39:52 -0300

    removendo dist

--- dist/out-tsc/broker-adapter/message.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Message = (function () {
-    function Message() {
-    }
-    return Message;
-}());
-exports.Message = Message;
-//# sourceMappingURL=message.js.map
--- dist/out-tsc/infrastructure/env.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Env = {
-    messageBrokerAddress: process.env['RABBITMQ_ADDRESS']
-};
-//# sourceMappingURL=env.js.map
